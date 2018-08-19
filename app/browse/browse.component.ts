d751b9203195c0cdcc1854f2ee30194096114f00
Advait <pundlik.advait51@gmail.com>
2018-08-19 21:17:48 +0530

    #3 Scanning service and shared Preferences  JSON parse is pending

--- app/browse/browse.component.ts
+++ app/browse/browse.component.ts
@@ -4,10 +4,6 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import { registerElement } from "nativescript-angular/element-registry";
 //import { TextField } from "../../node_modules/tns-core-modules/ui/text-field/text-field";
 
-
-
-registerElement("Fab", () => require("nativescript-floatingactionbutton").Fab);
-
 @Component({
     selector: "Browse",
     moduleId: module.id,
@@ -32,20 +28,74 @@ export class BrowseComponent implements OnInit  {
 
     save()
     {
+        console.log("hello");
         this.pref = require("nativescript-android-preferences");
-          var options = {
-            datavalue: "XORIANT",
-            datatype: this.pref.DataTypes.STRING,
-            prefname: "KEY"
-        }   
-       this.pref.SavePreference(options);
-       options.datavalue = "DefStringPreference";
-       console.log("hello");
+            var lname ={
+                datavalue: "",
+                datatype: this.pref.DataTypes.STRING,
+                prefname: "lname"
+               }
+            lname.datavalue="Doe"
+            this.pref.SavePreference(lname);
+            lname.datavalue = "DefStringPreference";
+
+           var fname ={
+                datavalue: "",
+                datatype: this.pref.DataTypes.STRING,
+                prefname: "fname"
+               }
+               fname.datavalue="john";
+               this.pref.SavePreference(fname);
+               fname.datavalue = "DefStringPreference";
+              
+                 var email ={
+                datavalue: "",
+                datatype: this.pref.DataTypes.STRING,
+                prefname: "email"
+               }   
+               email.datavalue="johndoe@example.com";
+               this.pref.SavePreference(email);
+               email.datavalue="DefStringPreference";
+
+               var ph1 ={
+                datavalue: "",
+                datatype: this.pref.DataTypes.STRING,
+                prefname: "ph1"
+               }
+                ph1.datavalue="9876543210";
+                this.pref.SavePreference(ph1);
+                ph1.datavalue="DefStringPreference";
+            console.log(this.pref.GetPreference(lname) +" "+ this.pref.GetPreference(fname)+ " " + this.pref.GetPreference(email) + " " +this.pref.GetPreference(ph1) );
    }
 
-   show(){
-      alert(this.pref.GetPreference("KEY")); 
-      console.log("hello");
+   show()
+   {
+   var fname ={
+        datavalue: "",
+        datatype: this.pref.DataTypes.STRING,
+        prefname: "fname"
+       }
+     var  lname ={
+        datavalue: "",
+        datatype: this.pref.DataTypes.STRING,
+        prefname: "lname"
+       }
+    
+      var email ={
+        datavalue: "",
+        datatype: this.pref.DataTypes.STRING,
+        prefname: "email"
+       }   
+
+       var ph1 ={
+        datavalue: "",
+        datatype: this.pref.DataTypes.STRING,
+        prefname: "ph1"
+       }
+    
+       
+      console.log(this.pref.GetPreference(lname) +" "+ this.pref.GetPreference(fname)+ " " + this.pref.GetPreference(email) + " " + this.pref.GetPreference(ph1));
+     
     }
    
 
--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import * as app from "application";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import { BarcodeScanner } from 'nativescript-barcodescanner';
+declare var android :any;
 class Contact {
     constructor(public name: string) { }
 }
@@ -18,6 +19,15 @@ export class HomeComponent implements OnInit {
     public contacts: Array<Contact>;
     
     constructor(private barcodeScanner: BarcodeScanner) {
+        var permissions = require('nativescript-permissions');
+        permissions.requestPermission(android.Manifest.permission.READ_CONTACTS, 
+        "<uses-permission android:name=\"android.permission.GET_ACCOUNTS\"/> <uses-permission android:name=\"android.permission.READ_CONTACTS\" /> <uses-permission android:name=\"android.permission.WRITE_CONTACTS\"/>")
+          .then(function() {
+             console.log("Woo Hoo, I have the power!");
+          })
+          .catch(function() {
+             console.log("Uh oh, no permissions - plan B time!");
+          });
         
     }
 
@@ -33,6 +43,8 @@ export class HomeComponent implements OnInit {
 
 
     fabTap(index): void {
+    
+  
         this.barcodeScanner.scan({
             cancelLabel: "Stop scanning",
             message: "Go scan something",
@@ -46,8 +58,8 @@ export class HomeComponent implements OnInit {
             var app = require( "application" );
             var contacts = require( "nativescript-contacts" );
             var newContact = new contacts.Contact();
-            newContact.name.given = "Advait ";
-            newContact.name.family = "Pundlik";
+            newContact.name.given = "AAdrak";
+            newContact.name.family = "LASAN";
             newContact.phoneNumbers.push({ label: contacts.KnownLabel.HOME, value: "000000000" }); // See below for known labels
             newContact.phoneNumbers.push({ label: "My Custom Label", value: "1111111111" });
             newContact.save();
@@ -71,5 +83,16 @@ export class HomeComponent implements OnInit {
         newContact.save();
         alert("Contact saved");
 
+    }
+
+    getpermissions(){
+        var permissions = require('nativescript-permissions');
+        permissions.requestPermission(android.Manifest.permission.READ_CONTACTS, "I need these permissions because I'm cool")
+     .then(function() {
+        console.log("Woo Hoo, I have the power!");
+       })
+     .catch(function() {
+      console.log("Uh oh, no permissions - plan B time!");
+      });
     }
 }
